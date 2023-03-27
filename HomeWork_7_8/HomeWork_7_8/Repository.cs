5e5bdd5cd3943b56230b0805d6efd6bed9b68203
Marlen Zhumakov <m_zhumakov@hotmail.com>
2023-03-27 17:20:49 +0600

    added worker struct and repository class

---
+++ HomeWork_7_8/HomeWork_7_8/Repository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_7_8
+{
+    internal class Repository
+    {
+        private string filePath = "employees.txt";
+        private char separator = '#';
+
+        public void AddWorker(Worker worker)
+        {
+            // присваиваем worker уникальный ID,
+            // дописываем нового worker в файл
+        }
+
+        public Worker[] GetAllWorkers() 
+        {
+            // здесь происходит чтение из файла
+            // и возврат массива считанных экземпляров
+            return new Worker[0]; 
+        }
+
+        public Worker GetWorkerById(int id)
+        {
+            // происходит чтение из файла, возвращается Worker
+            // с запрашиваемым ID
+            Worker[] workers = GetAllWorkers();
+            return workers[0];
+        }        
+
+        public Worker[] GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
+        {
+            // здесь происходит чтение из файла
+            // фильтрация нужных записей
+            // и возврат массива считанных экземпляров
+            Worker[] workers = GetAllWorkers();
+            return workers;
+        }
+
+        public void DeleteWorker(int id)
+        {
+            // считывается файл, находится нужный Worker
+            // происходит запись в файл всех Worker,
+            // кроме удаляемого
+        }
+    }
+}
