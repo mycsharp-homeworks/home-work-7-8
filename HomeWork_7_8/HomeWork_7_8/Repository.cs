fa8179404437ee03eb5708caf13fa01f18e7cd9e
Marlen Zhumakov <m_zhumakov@hotmail.com>
2023-03-28 19:37:15 +0600

    added functionality to the methods in Repository

--- HomeWork_7_8/HomeWork_7_8/Repository.cs
+++ HomeWork_7_8/HomeWork_7_8/Repository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,23 +16,59 @@ namespace HomeWork_7_8
 
         public void AddWorker(Worker worker)
         {
+            int idCounter = 0;
+
             // присваиваем worker уникальный ID,
+            worker.Id = ++idCounter;
+
             // дописываем нового worker в файл
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                string newWorker = $"{worker.Id}#{worker.FullName}#{worker.Age}#{worker.Height}#{worker.Birthday}#{worker.CityOfBirth}";
+                writer.WriteLine(newWorker);
+            }
         }
 
         public Worker[] GetAllWorkers() 
         {
             // здесь происходит чтение из файла
+            string[] linesOfWorkers = File.ReadAllLines(filePath);
+            Worker[] workers = new Worker[linesOfWorkers.Length];
+
+            for (int i = 0; i < workers.Length; i++)
+            {
+                string[] fields = linesOfWorkers[i].Split('#');
+                Worker worker = new Worker
+                {
+                    Id = Int32.Parse(fields[0]),
+                    FullName = fields[1],
+                    Age = Int32.Parse(fields[2]),
+                    Height = Int32.Parse(fields[3]),
+                    Birthday = DateTime.Parse(fields[4]),
+                    CityOfBirth = fields[5]
+                };
+                worker = workers[i];
+            }
+
             // и возврат массива считанных экземпляров
-            return new Worker[0]; 
+            return workers; 
         }
 
         public Worker GetWorkerById(int id)
         {
             // происходит чтение из файла, возвращается Worker
             // с запрашиваемым ID
-            Worker[] workers = GetAllWorkers();
-            return workers[0];
+            Worker[] allWorkers = GetAllWorkers();
+            Worker workerToReturn = new Worker();
+            
+            for (int i = 0; i < allWorkers.Length; i++)
+            {
+                if (allWorkers[i].Id == id)
+                {
+                    workerToReturn = allWorkers[i];
+                }
+            }
+            return workerToReturn;
         }        
 
         public Worker[] GetWorkersBetweenTwoDates(DateTime dateFrom, DateTime dateTo)
@@ -39,8 +76,20 @@ namespace HomeWork_7_8
             // здесь происходит чтение из файла
             // фильтрация нужных записей
             // и возврат массива считанных экземпляров
-            Worker[] workers = GetAllWorkers();
-            return workers;
+            int count = 0;
+            Worker[] allWorkers = GetAllWorkers();
+            Worker[] workersInBetween = new Worker[allWorkers.Length];
+
+            foreach (Worker worker in allWorkers)
+            {
+                if (worker.Birthday >= dateFrom && worker.Birthday <= dateTo)
+                {
+                    workersInBetween[count] = worker;
+                    count++;
+                }
+            }
+
+            return workersInBetween;
         }
 
         public void DeleteWorker(int id)
@@ -48,6 +97,24 @@ namespace HomeWork_7_8
             // считывается файл, находится нужный Worker
             // происходит запись в файл всех Worker,
             // кроме удаляемого
+            Worker[] allWorkers = GetAllWorkers();
+
+            string emptyContent = "";
+
+            File.WriteAllText(filePath, emptyContent);
+
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                for (int i = 0; i < allWorkers.Length; i++)
+                {
+                    if (allWorkers[i].Id == id)
+                    {
+                        continue;
+                    }
+                    string newWorker = $"{allWorkers[i].Id}#{allWorkers[i].FullName}#{allWorkers[i].Age}#{allWorkers[i].Height}#{allWorkers[i].Birthday}#{allWorkers[i].CityOfBirth}";
+                    writer.WriteLine(newWorker);
+                }                
+            }
         }
     }
 }
